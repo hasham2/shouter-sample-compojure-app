84884cb9a3b0fa30cff585701c12771dafbece49
Hasham Malik <hasham2@gmail.com>
2012-06-15 18:27:56 +0500

    updated core file

--- src/shouter/core.clj
+++ src/shouter/core.clj
@@ -6,46 +6,21 @@
 	      [shouter.controllers.shouts]
 	      [shouter.views.layout :as layout])
 
-;;lets see where this code goes
+(defroutes routes 
+	   shouter.contollers.shouts/routes
+	   (route/resources "/")
+	   (route/not-found (layout/four-oh-four)))
 
-(def testdata {
-      :date    "2012-01-01"
-      :body    "Happy New Year everyone"})
-
-(def db {
-      :classname	"org.sqlite.JDBC"
-      :subprotocol	"sqlite"
-      :subname		"db/database.db"
-      })
-
-(defn create-db []
-      (try (with-connection db 
-      	   (create-table :shouts 
-	   		 [:date :text] 
-			 [:body :text]))
-	(catch Exception e (println e)))
-)
-
-(create-db)
-
-(with-connection db 
-      (insert-records :shouts testdata))
-
-(def output 
-     (with-connection db 
-     		      (with-query-results rs ["select * from shouts"] 
-		      			  (doall rs))))
-(keys (first output))
-
-;; end code
+(def application (handler/site routes))
 
 (defn app [req]
       {:status 200
       :headers {"Content-Type" "text/plain"}
       :body "Hello World"})
 
-(defn start [] 
-      (ring/run-jetty #'routes {:port 8080 :join? false}))
+(defn start [port] 
+      (ring/run-jetty #'application {:port (or port 8080) :join? false}))
 
-;;(defn -main [port]
-;;      (run-jetty app {:port (Integer. port)}))
+(defn -main []
+      (let [port (Integer. 8000)]
+      (start port)))
